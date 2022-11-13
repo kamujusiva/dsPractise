1fd0d3b5467204a829658fa4b8a94a795fd84f4c
kamujusiva <86579168+kamujusiva@users.noreply.github.com>
2022-11-13 11:57:03 -0600

    Added LinkedListImplement.py

--- LinkedListImplement.py
+++ LinkedListImplement.py
@@ -1,5 +1,5 @@
 class Node:
-    def __int__(self, data):
+    def __init__(self, data):
         self.data = data
         self.next_node = None
 
@@ -7,7 +7,7 @@ class Node:
 class LinkedList:
 
     def __init__(self):
-        self.no_of_nodes = None
+        self.no_of_nodes = 0
         self.head = None
 
     def insert_start(self, data):
@@ -28,12 +28,17 @@ class LinkedList:
             while traversal_node.next_node is not None:
                 traversal_node = traversal_node.next_node
             traversal_node.next_node = new_node
+        self.no_of_nodes = self.no_of_nodes + 1
 
     def traverse(self):
+        print("Current Linked List Contains ", self.no_of_nodes, 'entries')
         traversal_node = self.head
         while traversal_node.next_node is not None:
-            print(traversal_node)
+            print("Value at Current Node:\t", traversal_node.data, '\t\tAddress is ', traversal_node
+                  , '\tNext Node Address', traversal_node.next_node)
             traversal_node = traversal_node.next_node
+        print("Value at Current Node:\t", traversal_node.data, '\t\tAddress is ', traversal_node, '\tNext Node Address'
+              , traversal_node.next_node)
 
     def remove_item(self, data):
         pass
@@ -43,5 +48,5 @@ linked_list = LinkedList()
 linked_list.insert_start(10)
 linked_list.insert_start(20)
 linked_list.insert_end('Adam')
-linked_list.insert_end('4.5')
+linked_list.insert_end(4.5)
 linked_list.traverse()
