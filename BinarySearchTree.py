06df6dc70808e0ba376e1338879a933a473bdca6
kamujusiva <86579168+kamujusiva@users.noreply.github.com>
2022-11-18 09:58:27 -0600

    Added LinkedListImplement.py and some code

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -64,11 +64,74 @@ class BinarySearchTree:
         if current_node.right_node is not None:
             self.traverse_in_order(current_node.right_node)
 
-    def display_binary_tree(self):
-        if self.root is None:
+    def remove(self, data):
+        if self.root is not None:
+            self.remove_node(data, self.root)
+
+    def remove_node(self, data, current_node: Node):
+        # First we have to find the node to remove
+        if current_node is None:
             return
+        elif data < current_node.data:
+            self.remove_node(data, current_node.left_node)
+        elif data > current_node.data:
+            self.remove_node(data, current_node.right_node)
         else:
-            pass
+            # we found the node to remove
+            # we have 3 options
+            # 1. node can be leaf node. this is easy
+            if current_node.left_node is None and current_node.right_node is None:
+                print("Removing Leaf Node: ", current_node.data)
+                parent = current_node.parent
+                if parent is not None and parent.left_node == current_node:
+                    parent.left_node = None
+                elif parent is not None and parent.right_node == current_node:
+                    parent.right_node = None
+                else:
+                    self.root = None
+                del current_node
+            # if there is a single right child
+            elif current_node.left_node is None and current_node.right_node is not None:
+                print("Removing the node with single right child: ", current_node.data)
+                parent = current_node.parent
+                if parent is not None and parent.left_node == current_node:
+                    parent.left_node = current_node.right_node
+                elif parent is not None and parent.right_node == current_node:
+                    parent.right_node = current_node.right_node
+                else:
+                    self.root = current_node.right_node
+                current_node.right_node.parent = parent
+                del current_node
+            # if there is single left child
+            elif current_node.left_node is not None and current_node.right_node is None:
+                print("Removing the node with single right child: ", current_node.data)
+                parent = current_node.parent
+                if parent is not None and parent.left_node == current_node:
+                    parent.left_node = current_node.left_node
+                elif parent is not None and parent.right_node == current_node:
+                    parent.right_node = current_node.left_node
+                else:
+                    self.root = current_node.left_node
+                current_node.left_node_node.parent = parent
+                del current_node
+            # Removing node with 2 children
+            else:
+                print("Removing node with 2 children: ", current_node.data)
+                predecessor = self.get_predecessor(current_node.left_node)
+                # Swap the current_node with predecessor
+                temp = predecessor.data
+                predecessor.data = current_node.data
+                current_node.data = temp
+
+                self.remove_node(data, predecessor)
+
+    def get_predecessor(self, current_node: Node):
+        if current_node.right_node is not None:
+            return self.get_predecessor(current_node.left_node)
+        return current_node
+
+
+
 
 
 bst = BinarySearchTree()
