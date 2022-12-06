bde01df4397dba4023146d795a6c3fc7f38eb691
kamujusiva <86579168+kamujusiva@users.noreply.github.com>
2022-12-06 10:43:33 -0600

    updated code

--- ShoartestPath.py
+++ ShoartestPath.py
@@ -21,3 +21,38 @@ def build_distance_table(graph: AdjacencyMatrixGraph, source):
             # Only update the distance table if no current distance from the source is set
             if distance_table[neighbor][0] is None:
                 distance_table[neighbor] = (1 + current_distance, current_vertex)
+                # Enqueue the neighbor only if it has other adjacent vertices to explore
+                if len(graph.get_adjacent_vertices(neighbor)) > 0:
+                    queue.put(neighbor)
+
+    return distance_table
+
+
+def shortest_path(graph: AdjacencyMatrixGraph, source, destination):
+    distance_table = build_distance_table(graph, source)
+    path = [destination]
+    previous_vertex = distance_table[destination][1]
+    while previous_vertex is not None and previous_vertex is not source:
+        path = [previous_vertex] + path
+        previous_vertex = distance_table[previous_vertex][1]
+        if previous_vertex is None:
+            print("There is no path from %d to %d" % (source, destination))
+        else:
+            path = [source] + path
+            print("Shortest Path is: ", path)
+
+
+g = AdjacencyMatrixGraph(8, directed=True)
+g.add_edge(0, 1)
+g.add_edge(1, 2)
+g.add_edge(1, 3)
+g.add_edge(2, 3)
+g.add_edge(1, 4)
+g.add_edge(3, 5)
+g.add_edge(5, 4)
+g.add_edge(3, 6)
+g.add_edge(6, 7)
+g.add_edge(0, 7)
+shortest_path(g, 0, 5)
+shortest_path(g, 0, 6)
+shortest_path(g, 7, 4)
